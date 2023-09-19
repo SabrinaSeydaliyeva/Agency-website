7034e3e2126b9df4762d3d056e2fa04a552deb2c
SabrinaSeydaliyeva <seydaliyeva.sabrina@bk.ru>
2023-09-19 14:50:19 +0400

    .

--- pages/loginPage/index.js
+++ pages/loginPage/index.js
@@ -1,8 +1,15 @@
-let userName= document.querySelector('.name-input')
-let password= document.querySelector('.password-input')
-let submitBtn=document.querySelector("button")
-submitBtn.addEventListener('click', handleLogin)
-function handleLogin(e){
-    e.preventDefault()
-    console.log('saaa');
+let userName = document.querySelector(".name-input");
+let password = document.querySelector(".password-input");
+let submitBtn = document.querySelector("button");
+submitBtn.addEventListener("submit", handleLogin);
+function handleLogin(e) {
+  e.preventDefault();
+  if (userName.value && password.value) {
+    
+
+    console.log(userName.value);
+    console.log(password.value);
+  } else {
+    
+  }
 }
--- pages/registerPage/index.js
+++ pages/registerPage/index.js
@@ -1,24 +1,49 @@
-let form=document.querySelector("form")
-let emailInput=document.querySelector(".email-input")
-let nameInput=document.querySelector(".name-input")
-let passwordInput=document.querySelector(".password-input")
+let form = document.querySelector("form");
+let emailInput = document.querySelector(".email-input");
+let nameInput = document.querySelector(".name-input");
+let passwordInput = document.querySelector(".password-input");
+let btn = document.querySelector("button");
+let errorMsj=document.querySelector(".error-msj")
 
-form.addEventListener("submit", handleRegister)
-let dataBase=[]
+
+form.addEventListener("submit", handleRegister);
+let dataBase = [];
 function handleRegister(e) {
-e.preventDefault()
-console.log(emailInput.value);
-console.log(nameInput.value);
-console.log(passwordInput.value);
+  e.preventDefault();
+  if (emailInput.value && nameInput.value && passwordInput.value) {
+    console.log(emailInput.value);
+    console.log(nameInput.value);
+    console.log(passwordInput.value);
+  } else {
+  }
 
-let user ={
+  let user = {
     email: emailInput.value,
     name: nameInput.value,
     password: passwordInput.value,
+  };
+  dataBase.push(user);
+  form.reset();
+  console.log(dataBase);
 }
-dataBase.push(user)
-form.reset()
-console.log(dataBase);
 
+let allInputs = document.querySelectorAll("input");
+console.log(allInputs);
+allInputs.forEach((element) => {
+  element.addEventListener("keyup", clickMe);
+});
+function clickMe (e) {
+    if (emailInput.value && nameInput.value && passwordInput.value) {
+
+    btn.style.background="red";
+    btn.disabled=false;
+    errorMsj.style.display = "none";
+
+    }
+    else {
+        btn.style.background="gray";
+        btn.disabled=true;
+        errorMsj.style.display = "block";
+
+    }
 }
-
